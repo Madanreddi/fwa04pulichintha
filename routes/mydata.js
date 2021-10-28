93c8f38a1fb2fc94b8e134f187fa891fc237838e
Madanreddi <89476569+Madanreddi@users.noreply.github.com>
2021-10-27 21:26:42 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'madan' });
+  res.render('mydata', { title: 'Madan Mohan Reddy Pulichintha' });
 });
 
 module.exports = router;
